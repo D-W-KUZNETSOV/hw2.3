98b0dd47391b04b784601586086dba3cca686365
Dmitryi Kuznetsov <d.w.kuznetsov@yandex.ru>
2024-07-07 08:25:01 +0300

    calculator

--- src/main/java/pro/sky/calculator/CalculatorController.java
+++ src/main/java/pro/sky/calculator/CalculatorController.java
@@ -9,37 +9,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/calculator")
 
-public class CalculatorController {
+public class CalculatorController  {
+
 
   private final CalculatorService calculatorService;
 
-  public CalculatorController(CalculatorService calculatorService) {
+  public CalculatorController(CalculatorService calculatorService)  {
     this.calculatorService = calculatorService;
   }
-
   @GetMapping
   public String helloCalculator() {
     return calculatorService.helloCalculator();
   }
-
-
   @GetMapping("/plus")
   public String plus(@RequestParam int num1,
       @RequestParam int num2) {
 
     return calculatorService.plus(num1, num2);
   }
-
   @GetMapping("/minus")
   public String minus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
     return calculatorService.minus(num1, num2);
   }
-
   @GetMapping("/multiply")
   public String multiply(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
     return calculatorService.multiply(num1, num2);
   }
-
   @GetMapping("/divide")
   public String divide(@RequestParam("num1") double num1, @RequestParam("num2") double num2) {
     return calculatorService.divide(num1, num2);
--- src/main/java/pro/sky/calculator/CalculatorService.java
+++ src/main/java/pro/sky/calculator/CalculatorService.java
@@ -1,37 +1,26 @@
 package pro.sky.calculator;
-
 import org.springframework.stereotype.Service;
-
 
 @Service
 public class CalculatorService {
 
-
   public String helloCalculator() {
     return "Добро пожаловать в калькулятор";
   }
-
-
   public String plus(double num1, double num2) {
 
     double rezult = num1 + num2;
     return num1 + " + " + num2 + " = " + rezult;
 
   }
-
-
   public String minus(double num1, double num2) {
     double rezult = num1 - num2;
     return num1 + " - " + num2 + " = " + rezult;
   }
-
-
   public String multiply(double num1, double num2) {
     double rezult = num1 * num2;
     return num1 + " * " + num2 + " = " + rezult;
   }
-
-
   public String divide(double num1, double num2) {
 
     if (num2 != 0) {
@@ -39,8 +28,10 @@ public class CalculatorService {
       return num1 + " / " + num2 + " = " + rezult;
     } else {
       return "Ошибка,делить на ноль нельзя";
+
     }
 
 
   }
+
 }
