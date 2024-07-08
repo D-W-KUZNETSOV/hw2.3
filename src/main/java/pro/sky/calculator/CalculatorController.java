87c371c6701f26dc185ba4931c2a8e72fe4a974e
Dmitryi Kuznetsov <d.w.kuznetsov@yandex.ru>
2024-07-08 17:35:53 +0300

    calculator

--- src/main/java/pro/sky/calculator/CalculatorController.java
+++ src/main/java/pro/sky/calculator/CalculatorController.java
@@ -1,7 +1,6 @@
 package pro.sky.calculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,35 +8,46 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/calculator")
 
-public class CalculatorController  {
+public class CalculatorController {
 
 
   private final CalculatorService calculatorService;
 
-  public CalculatorController(CalculatorService calculatorService)  {
+  public CalculatorController(CalculatorService calculatorService) {
     this.calculatorService = calculatorService;
   }
+
   @GetMapping
   public String helloCalculator() {
     return calculatorService.helloCalculator();
   }
-  @GetMapping("/plus")
-  public String plus(@RequestParam int num1,
-      @RequestParam int num2) {
 
-    return calculatorService.plus(num1, num2);
+  @GetMapping("/plus")
+  public String plus(@RequestParam("num1") int num1,
+      @RequestParam("num2") int num2) {
+    var rezult = calculatorService.plus(num1, num2);
+    return num1 + " + " + num2 + " = " + rezult;
   }
+
   @GetMapping("/minus")
   public String minus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-    return calculatorService.minus(num1, num2);
+    var rezult = calculatorService.minus(num1, num2);
+    return num1 + " - " + num2 + " = " + rezult;
   }
+
   @GetMapping("/multiply")
   public String multiply(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-    return calculatorService.multiply(num1, num2);
+    var rezult = calculatorService.multiply(num1, num2);
+    return num1 + " * " + num2 + " = " + rezult;
   }
+
   @GetMapping("/divide")
-  public String divide(@RequestParam("num1") double num1, @RequestParam("num2") double num2) {
-    return calculatorService.divide(num1, num2);
+  public String divide(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+    var rezult = calculatorService.divide(num1,num2);
+    if (num2 == 0) {
+      return "Ошибка,делить на ноль нельзя";
+    }
+    return num1 + " / " + num2 + " = " + rezult;
   }
 
 
--- src/main/java/pro/sky/calculator/CalculatorService.java
+++ src/main/java/pro/sky/calculator/CalculatorService.java
@@ -1,4 +1,5 @@
 package pro.sky.calculator;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,31 +8,23 @@ public class CalculatorService {
   public String helloCalculator() {
     return "Добро пожаловать в калькулятор";
   }
-  public String plus(double num1, double num2) {
 
-    double rezult = num1 + num2;
-    return num1 + " + " + num2 + " = " + rezult;
-
-  }
-  public String minus(double num1, double num2) {
-    double rezult = num1 - num2;
-    return num1 + " - " + num2 + " = " + rezult;
-  }
-  public String multiply(double num1, double num2) {
-    double rezult = num1 * num2;
-    return num1 + " * " + num2 + " = " + rezult;
-  }
-  public String divide(double num1, double num2) {
-
-    if (num2 != 0) {
-      double rezult = num1 / num2;
-      return num1 + " / " + num2 + " = " + rezult;
-    } else {
-      return "Ошибка,делить на ноль нельзя";
-
-    }
-
-
+  public int plus(int num1, int num2) {
+    return num1 + num2;
   }
 
+  public int minus(int num1, int num2) {
+    return num1 - num2;
+  }
+
+  public int multiply(int num1, int num2) {
+    return num1 * num2;
+  }
+
+  public int divide(int num1, int num2) {
+    return num1 / num2;
+
+  }
 }
+
+
